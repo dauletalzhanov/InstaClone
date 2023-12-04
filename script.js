4e491b0e128197809fd579afc8f9621604ddf97b
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-12-04 19:23:06 +0000

    more dropside

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 body = document.querySelector("body")
-//body.style.cssText = "background-color:pink;"
+
+
+more = document.querySelector(".more")
+moreContent = document.querySelector(".moreDropDownList")
+darkMode = document.querySelector(".darkMode")
+lightsOut = document.querySelector(".lightsOut")
+
+more.addEventListener("click", function(){
+	moreContent.style.display = "none"
+
+	if (moreContent.style.display === 'none'){
+		moreContent.style.display = "flex"
+	} else {
+		moreContent.style.display = "none"
+	}
+})
+
+
+darkMode.addEventListener("click", function(){
+	moreContent.style.display = "none"
+})
+
+lightsOut.addEventListener("click", function(){
+	moreContent.style.display = "none"
+})
