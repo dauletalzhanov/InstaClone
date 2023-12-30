f8696a0c738e822b2cece6f2f4d84e92c1b4e969
Daulet <35165187+dauletalzhanov@users.noreply.github.com>
2023-12-30 19:31:11 +0000

    added comment layout

--- John Doe/profile.js
+++ John Doe/profile.js
@@ -1,5 +1,9 @@
 bohemian = document.querySelector(".img1")
-
 bohemian.addEventListener("click", function(){
 	window.location="./img1/img1.html"
+})
+
+giraffe = document.querySelector(".img2")
+giraffe.addEventListener("click", function(){
+	window.location="./img2/img2.html"
 })
