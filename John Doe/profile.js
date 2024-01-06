649506eb156b0a2fbd0987a2719400e41bab44f7
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-01-06 12:47:17 +0000

    added a third picture for john's profile

--- John Doe/profile.js
+++ John Doe/profile.js
@@ -7,3 +7,8 @@ giraffe = document.querySelector(".img2")
 giraffe.addEventListener("click", function(){
 	window.location="./img2/img2.html"
 })
+
+italy = document.querySelector(".img3")
+italy.addEventListener("click", function(){
+	window.location="./img3/img3.html"
+})
